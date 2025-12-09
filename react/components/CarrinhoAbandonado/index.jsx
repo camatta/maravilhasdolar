11cc0758ff92004f6dbdceeae45924d860f3bd5d
devnairuz <desenvolvimentonairuz@gmail.com>
2025-12-09 10:58:30 -0300

    alteração do endpoint para railway

--- react/components/CarrinhoAbandonado/index.jsx
+++ react/components/CarrinhoAbandonado/index.jsx
@@ -71,7 +71,7 @@ function CarrinhoAbandonado() {
             );
 
             // Enviar para o middleware
-            const response = await fetch(`https://middlewares-maravilhas.vercel.app/cart/track`, {
+            const response = await fetch(`https://middlewares-maravilhas.railway.internal/cart/track`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
